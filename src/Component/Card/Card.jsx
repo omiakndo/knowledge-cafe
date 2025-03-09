0c00a4f1dd550a77bf74cd583c957cb5b6686f30
omiakando <shohalakndo@gmail.com>
2025-03-09 20:10:11 +0600

    all-card

--- src/Component/Card/Card.jsx
+++ src/Component/Card/Card.jsx
@@ -21,7 +21,7 @@ const Card = () => {
                     </div>
 
                     <h1 className='dis'>How to get your first job as a <br /> self-taught programmer</h1>
-                    <hr className='hr-pr'/>
+                    <hr className='hr-pr' />
                 </div>
 
 
@@ -29,12 +29,66 @@ const Card = () => {
             </div>
             <div>
                 <img className='img-card' src="/images/1.jpg" alt="" />
+                <div>
+
+                    <div className='full-pr'>
+                        <div className='pr'>
+                            <img className='img' src="/images/boy2.png" alt="" />
+                            <div>
+                                <h1 className='name'>Mrs. Setara</h1>
+                                <p className='active'>Mar 14 (4 Days ago)</p>
+                            </div>
+                        </div>
+                        <div>
+                            <p className='time'> 07 min read</p>
+                        </div>
+                    </div>
+
+                    <h1 className='dis'>How to get your first job as a <br /> self-taught programmer</h1>
+                    <hr className='hr-pr' />
+                </div>
             </div>
             <div>
                 <img className='img-card' src="/images/3.png" alt="" />
+                <div>
+
+                    <div className='full-pr'>
+                        <div className='pr'>
+                            <img className='img' src="/images/girl2.jpg" alt="" />
+                            <div>
+                                <h1 className='name'>Mina Mostofa</h1>
+                                <p className='active'>Mar 14 (4 Days ago)</p>
+                            </div>
+                        </div>
+                        <div>
+                            <p className='time'> 10 min read</p>
+                        </div>
+                    </div>
+
+                    <h1 className='dis'>How to get your first job as a <br /> self-taught programmer</h1>
+                    <hr className='hr-pr' />
+                </div>
             </div>
             <div>
                 <img className='img-card' src="/images/4.jpg" alt="" />
+                <div>
+
+                    <div className='full-pr'>
+                        <div className='pr'>
+                            <img className='img' src="/images/girl1.jpg" alt="" />
+                            <div>
+                                <h1 className='name'>Mr. Raju</h1>
+                                <p className='active'>Mar 14 (4 Days ago)</p>
+                            </div>
+                        </div>
+                        <div>
+                            <p className='time'> 15 min read</p>
+                        </div>
+                    </div>
+
+                    <h1 className='dis'>How to get your first job as a <br /> self-taught programmer</h1>
+                    <hr className='hr-pr' />
+                </div>
             </div>
 
         </div>
